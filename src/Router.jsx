b4ccf348b635b03aa4781c877161444d6ebf1289
tsukuga <51287376+tsukuga@users.noreply.github.com>
2026-04-27 18:24:22 +0900

    Fix HashRouter import to use react-router instead of react-router-dom

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, HashRouter } from "react-router-dom";
+import { Route, Switch, HashRouter } from "react-router";
 import Ello from './templates/Ello'
 
 const Router = () => {
