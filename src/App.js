2f4c875419be507fe9cf7df2b46ba32499771438
tsukuga <51287376+tsukuga@users.noreply.github.com>
2026-04-27 17:38:14 +0900

    Move App.js to src/App.js for Create React App structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Router from "../Router";
+
+const App = () => {
+    return (
+            <main className="c-main">
+                <Router/>
+            </main>
+    );
+};
+
+export default App;
