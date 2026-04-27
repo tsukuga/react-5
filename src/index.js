748cc43c249dd0696b52346ccf120ab0f0e6a6dd
tsukuga <51287376+tsukuga@users.noreply.github.com>
2026-04-27 17:38:26 +0900

    Move index.js to src/index.js for Create React App structure

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import * as History from 'history';
+import createStore from '../reducks/store/store';
+import App from './App';
+
+import '../style.css';
+
+const history = History.createBrowserHistory();
+export const store = createStore(history);
+
+render(
+    <Provider store={store}>
+    <ConnectedRouter history={history}>
+    <App/>
+    </ConnectedRouter>
+    </Provider>,
+    document.getElementById('root'),
+)
